e2d972e7aaafd78bf880ceb780e6ca6a02609b9a
albahernandez-96 <alba.hernandez@allstacks.com>
2026-07-17 12:08:04 -0600

    Add sample Cypress ecommerce tests

---
+++ cypress/e2e/checkout.cy.js
@@ -0,0 +1,30 @@
+describe('Checkout Flow', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/');
+
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+  });
+
+  it('should complete the checkout process', () => {
+
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+    cy.get('.shopping_cart_link').click();
+
+    cy.get('[data-test="checkout"]').click();
+
+    cy.get('[data-test="firstName"]').type('Alba');
+    cy.get('[data-test="lastName"]').type('Hernandez');
+    cy.get('[data-test="postalCode"]').type('64000');
+
+    cy.get('[data-test="continue"]').click();
+
+    cy.get('[data-test="finish"]').click();
+
+    cy.contains('Thank you for your order!').should('be.visible');
+  });
+
+});
